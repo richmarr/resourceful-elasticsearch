31aa02a083e06276eab92af269e076a2a145a240
Richard Marr <richard.marr@gmail.com>
2012-01-14 09:17:05 +0000

    Slightly more forgiving support for ES query objects

---
+++ test/params-test.js
@@ -0,0 +1,80 @@
+var assert = require('assert'),
+	events = require('events'),
+	vows = require('vows'),
+	resourceful = require('resourceful'),
+	esEngine = require('../index'),
+	Elasticsearch = esEngine.Elasticsearch;
+
+resourceful.env = 'test';
+resourceful.use('elasticsearch',{index:'resourceful-test'});
+
+var Article; // Set this in module scope so we can refer to it a few times
+
+vows.describe('Parameter parsing').addBatch({
+	"Query param": {
+		"String":{
+			topic: function () {
+				return esEngine.generateQuery("red fox");
+			},
+			"should result in a query_string query":function( obj ){
+				assert.equal( obj.query.query_string.query, "red fox" );
+			}
+		},
+		"Query definition only":{
+			topic: function () {
+				return esEngine.generateQuery({
+					term:{
+						terms:"fox"
+					}
+				});
+			},
+			"should result in a term query":function( obj ){
+				assert.equal( obj.query.term.terms, "fox" );
+			}
+		},
+		"Full ES query object":{
+			topic: function () {
+				return esEngine.generateQuery({
+					query:{
+						term:{
+							terms:"fox"
+						}
+					},
+					filter:{
+						term:{ foo:'bar' }
+					}
+				});
+			},
+			"should contain a term query":function( obj ){
+				assert.equal( obj.query.term.terms, "fox" );
+			},
+			"should still contain its filter":function( obj ){
+				assert.equal( obj.filter.term.foo, "bar" );
+			}
+		}
+	}
+}).addBatch({
+	"Response processing": {
+		topic: function(){
+			return esEngine.getDocs({
+				total: 1,
+				max_score: 0.081366636,
+				hits:[
+					{
+						_index: 'resourceful-test',
+						_type: 'Article',
+						_id: 'mEB1nyFoTPu6tJi-FXL4Jg',
+						_score: 0.081366636,
+						_source: { title:'smeg', resource:'Article' }
+					}
+				]
+			});
+		},
+		"should provide one hit":function( results ){
+			assert.equal( results.length, 1 );
+		},
+		"hits should be valid Resource data":function( results ){
+			assert.equal( results[0].resource, "Article" );
+		}
+	}
+}).export(module);
--- test/search-test.js
+++ test/search-test.js
@@ -1,65 +1,67 @@
 var assert = require('assert'),
-    events = require('events'),
-    elastical = require('elastical'),
-    vows = require('vows'),
-    resourceful = require('resourceful'),
-    Elasticsearch = require('../index').Elasticsearch;
+	events = require('events'),
+	elastical = require('elastical'),
+	vows = require('vows'),
+	resourceful = require('resourceful'),
+	Elasticsearch = require('../index').Elasticsearch;
+
+var testIndexName = 'resourceful-easticsearch-test';
 
 resourceful.env = 'test';
-resourceful.use('elasticsearch',{index:'resourceful-test'});
+resourceful.use('elasticsearch',{index:testIndexName});
 
 var Article; // Set this in module scope so we can refer to it a few times
 
-vows.describe('resourceful/elasticsearch/search').addBatch({
-  "An index containing articles and other resources": {
-    topic: function () {
-      var that = this;
-      var client = new(elastical.Client)('127.0.0.1', {port:9200});
-      client.deleteIndex('resourceful-test', function () {
-        client.createIndex('resourceful-test', function () {
-          client.bulk([
-            {index:{ index:'resourceful-test', type:'Article', data:{ resource: 'Article', title: 'The Great Gatsby', published: true,  author: 'fitzgerald', tags: ['classic'] }}},
-            {index:{ index:'resourceful-test', type:'Article', data:{ resource: 'Article', title: 'Finding vim',      published: false, author: 'cloudhead', tags: ['hacking', 'vi'] }}},
-            {index:{ index:'resourceful-test', type:'Article', data:{ resource: 'Article', title: 'On Writing',       published: true,  author: 'cloudhead', tags: ['writing'] }}},
-            {index:{ index:'resourceful-test', type:'Article', data:{ resource: 'Article', title: 'vi Zen',           published: false, author: 'cloudhead', tags: ['vi', 'zen'] }}},
-            {index:{ index:'resourceful-test', type:'Article', data:{ resource: 'Article', title: 'Channeling force', published: true,  author: 'yoda',      tags: ['force', 'zen'] }}},
-            {index:{ index:'resourceful-test', type:'Body', data:{ resource: 'Body',    name: 'fitzgerald' }}},
-            {index:{ index:'resourceful-test', type:'Herring', data:{ resource: 'Herring',    name: 'Red', tags:['vim'] }}}
-          ], function (err,res) {
-            that.callback( err );
-          });
-        });
-      })//*/that.callback();
-    },
-    "is created": function () {}
-  }
+vows.describe('Indexing and Search').addBatch({
+	"An index containing articles and other resources": {
+		topic: function () {
+			var that = this;
+			var client = new(elastical.Client)('127.0.0.1', {port:9200});
+			client.deleteIndex( testIndexName, function () {
+				client.createIndex( testIndexName, function () {
+					client.bulk([
+						{index:{ index:testIndexName, type:'Article', data:{ resource: 'Article', title: 'The Great Gatsby', published: true,  author: 'fitzgerald', tags: ['classic'] }}},
+						{index:{ index:testIndexName, type:'Article', data:{ resource: 'Article', title: 'Finding vim',      published: false, author: 'cloudhead', tags: ['hacking', 'vi'] }}},
+						{index:{ index:testIndexName, type:'Article', data:{ resource: 'Article', title: 'On Writing',       published: true,  author: 'cloudhead', tags: ['writing'] }}},
+						{index:{ index:testIndexName, type:'Article', data:{ resource: 'Article', title: 'vi Zen',           published: false, author: 'cloudhead', tags: ['vi', 'zen'] }}},
+						{index:{ index:testIndexName, type:'Article', data:{ resource: 'Article', title: 'Channeling force', published: true,  author: 'yoda',      tags: ['force', 'zen'] }}},
+						{index:{ index:testIndexName, type:'Body', data:{ resource: 'Body',    name: 'fitzgerald' }}},
+						{index:{ index:testIndexName, type:'Herring', data:{ resource: 'Herring',    name: 'Red', tags:['vim'] }}}
+					], function (err,res) {
+						that.callback( err );
+					});
+				});
+			});
+		},
+		"is created": function () {}
+	}
 }).addBatch({
-  "Searching the index": {
-    topic: function () {
-      var that = this;
-      Article = resourceful.define('Article', function () {
-        this.property('author');
-        this.property('title');
-        this.property('tags');
-        this.property('published', Boolean);
-      });
-      setTimeout(function(){
-        that.callback();
-      },1000); // HACK - there's a delay before these documents are available for search so I'm delaying this
-    },
-    "for a simple search term":{
-      topic:function(){
-        Article.search("vim",this.callback);
-      },
-      "should return results":function( err, resources ){
-        assert.equal( resources.length, 1 );
-      },
-      "results should be valid Article objects":function( err, resources ){
-        assert.equal( resources[0].resource, "Article" );
-      },
-      "results should be the one expected":function( err, resources ){
-        assert.equal( resources[0].title, "Finding vim" );
-      }
-    }
-  }
+	"Searching the index": {
+		topic: function () {
+			var that = this;
+			Article = resourceful.define('Article', function () {
+				this.property('author');
+				this.property('title');
+				this.property('tags');
+				this.property('published', Boolean);
+			});
+			setTimeout(function(){
+				that.callback();
+			},1000); // HACK - there's a delay before these documents are available for search so I'm delaying this
+		},
+		"for a simple search term":{
+			topic:function(){
+				Article.search("vim",this.callback);
+			},
+			"should return results":function( err, resources ){
+				assert.equal( resources.length, 1 );
+			},
+			"results should be valid Article objects":function( err, resources ){
+				assert.equal( resources[0].resource, "Article" );
+			},
+			"results should be the one expected":function( err, resources ){
+				assert.equal( resources[0].title, "Finding vim" );
+			}
+		}
+	}
 }).export(module);
